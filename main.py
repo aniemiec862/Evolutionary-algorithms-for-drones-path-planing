55ceef8f77c0f8076687ff72332302332241c65e
Adam <adamniemiec862@gmail.com>
2023-11-03 22:40:10 +0100

    Moving based on angles and speed

--- main.py
+++ main.py
@@ -2,34 +2,34 @@ from uav.uav import UAV, Position
 from map.map import Map, MapObjectType, MapObject
 
 if __name__ == "__main__":
-    uav = UAV(Position(3, 3))
-    uav.move(Position(10, 10))
-    uav.move(Position(30, 18))
-    uav.move(Position(38, 35))
-    uav.move(Position(30, 47))
-    uav.move(Position(40, 80))
-    uav.move(Position(68, 75))
-    uav.move(Position(95, 95))
+    uav = UAV(Position(30, 30), 3, 5, 305, 0.2)
+    uav.move_to_position(Position(100, 100))
+    uav.move_to_position(Position(300, 180))
+    uav.move_to_position(Position(380, 350))
+    uav.move_to_position(Position(300, 470))
+    uav.move_to_position(Position(400, 800))
+    uav.move_to_position(Position(680, 750))
+    uav.move_to_position(Position(910, 920))
 
-    uav2 = UAV(Position(3, 3))
-    uav2.move(Position(20, 5))
-    uav2.move(Position(40, 8))
-    uav2.move(Position(58, 21))
-    uav2.move(Position(75, 38))
-    uav2.move(Position(87, 59))
-    uav2.move(Position(90, 65))
+    uav2 = UAV(Position(30, 30), 3, 5, 305, 0.2)
+    uav2.move_to_position(Position(200, 50))
+    uav2.move_to_position(Position(400, 80))
+    uav2.move_to_position(Position(580, 210))
+    uav2.move_to_position(Position(750, 380))
+    uav2.move_to_position(Position(870, 590))
+    uav2.move_to_position(Position(900, 650))
 
     points = [
-        MapObject(57, 40, 10, MapObjectType.OBSTACLE),
-        MapObject(84, 20, 6, MapObjectType.OBSTACLE),
-        MapObject(40, 15, 5, MapObjectType.OBSTACLE),
-        MapObject(18, 36, 12, MapObjectType.OBSTACLE),
-        MapObject(20, 80, 15, MapObjectType.OBSTACLE),
-        MapObject(50, 55, 8, MapObjectType.OBSTACLE),
-        MapObject(85, 70, 7, MapObjectType.OBSTACLE),
-        MapObject(5, 5, 5, MapObjectType.START),
-        MapObject(95, 95, 5, MapObjectType.OBJECTIVE)
+        MapObject(570, 400, 100, MapObjectType.OBSTACLE),
+        MapObject(840, 200, 60, MapObjectType.OBSTACLE),
+        MapObject(400, 150, 50, MapObjectType.OBSTACLE),
+        MapObject(180, 360, 120, MapObjectType.OBSTACLE),
+        MapObject(200, 800, 150, MapObjectType.OBSTACLE),
+        MapObject(500, 550, 80, MapObjectType.OBSTACLE),
+        MapObject(850, 700, 70, MapObjectType.OBSTACLE),
+        MapObject(50, 50, 50, MapObjectType.START),
+        MapObject(950, 950, 50, MapObjectType.OBJECTIVE)
     ]
 
-    my_map = Map(100, 100, points)
+    my_map = Map(1000, 1000, points)
     my_map.visualize([uav, uav2])
--- uav/uav.py
+++ uav/uav.py
@@ -1,21 +1,50 @@
-from math import sqrt
+from math import sqrt, sin, cos, radians
 
 class Position:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
+class Genotype:
+    def __init__(self, velocity, maneuverability):
+        self.velocity = velocity
+        self.maneuverability = maneuverability
+
 class UAV:
-    def __init__(self, position: Position):
+    def __init__(self, position: Position, velocity: float, max_velocity: float, angle: float, maneuverability: float):
         self.current_position = position
         self.moves = [position]
+        self.velocity = velocity
+        self.max_velocity = max_velocity
+        self.angle = angle
+        self.maneuverability = maneuverability
 
-    def move(self, position: Position):
+    def move_to_position(self, position: Position):
         self.moves.append(position)
         self.current_position = position
 
+    def move(self):
+        new_x = self.current_position.x + self.velocity * cos(radians(self.angle))
+        new_y = self.current_position.y + self.velocity * sin(radians(self.angle))
+        new_position = Position(new_x, new_y)
+
+        self.current_position = new_position
+        self.moves.append(new_position)
+
+    def turn(self, direction):
+        if direction == "left":
+            self.angle -= self.velocity * self.maneuverability
+        elif direction == "right":
+            self.angle += self.velocity * self.maneuverability
+
+        self.angle %= 360
+
     def get_moves(self):
         return self.moves
+
+    def set_velocity(self, v: float):
+        if v <= self.max_velocity:
+            self.velocity = v
 
     def calculate_traveled_distance(self):
         distance = 0.0
